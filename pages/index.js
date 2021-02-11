7815f606775079158edf85803532ba6ff1aac0dd
Jennifer Bird <jenniredfield@hotmail.com>
2021-02-11 20:22:52 +0000

    Renamed layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/wrapper';
 import utilStyles from '../styles/utils.module.css'
 import { getAllMDXPosts } from '../lib/posts'
 import Date from '../components/date'
