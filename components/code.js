dc1337b25ca745fb29fd8fc4921d8cedb3d918e3
Jennifer Bird <jenniredfield@hotmail.com>
2021-06-01 13:37:04 +0100

    Updated express-serverless post

--- components/code.js
+++ components/code.js
@@ -3,7 +3,6 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from "prism-react-renderer/themes/nightOwl";
 
 export default ({ children }) => {
-  console.log("🚀 ~ file: code.js ~ line 6 ~ children", children)
   const lang = children?.props.className ? children.props.className.replace('language-', '') : ''
 
   return (
@@ -11,7 +10,7 @@ export default ({ children }) => {
       {({className, style, tokens, getLineProps, getTokenProps}) => (
         <pre className={className} style={{...style, padding: '20px', fontSize: '16px', margin: '60px 0', position: 'relative'}}>
 
-          <button style={{position: 'absolute', top: '10px', right: '20px'}}>Click</button>
+          {/* <button style={{position: 'absolute', top: '10px', right: '20px'}}>Click</button> */}
 
           {tokens.slice(0, tokens.length - 1).map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
