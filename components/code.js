2879c07b7b3a854ceae6c6d9150b637af9a0b9f5
Jennifer Bird <jenniredfield@hotmail.com>
2021-06-01 13:31:51 +0100

    Create post deploy express to aws with serverless

--- components/code.js
+++ components/code.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from "prism-react-renderer/themes/nightOwl";
 
-export default ({children}) => {
+export default ({ children }) => {
+  console.log("🚀 ~ file: code.js ~ line 6 ~ children", children)
   const lang = children?.props.className ? children.props.className.replace('language-', '') : ''
+
   return (
     <Highlight {...defaultProps} code={children.props.children} language={lang} theme={theme}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px', fontSize: '16px', margin: '60px 0'}}>
+        <pre className={className} style={{...style, padding: '20px', fontSize: '16px', margin: '60px 0', position: 'relative'}}>
+
+          <button style={{position: 'absolute', top: '10px', right: '20px'}}>Click</button>
+
           {tokens.slice(0, tokens.length - 1).map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
               {line.map((token, key) => (
